c8ab4bc62b116da2eebfe7fab0ab7fa5977fe3fc
Gagandeep Singh <gagan.sngh14@gmail.com>
2022-08-23 16:55:39 +0530

    Application name change

--- src/main/java/me/gagan/service/BooksServiceApplication.java
+++ src/main/java/me/gagan/service/BooksServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookServiceApplication {
+public class BooksServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BookServiceApplication.class, args);
+        SpringApplication.run(BooksServiceApplication.class, args);
     }
 }
